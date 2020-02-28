179108b50373770a2bfd9dbb95a0ddc9dbc2d544
shubhaliharode <shubhaliharode111@gmail.com>
2020-02-28 12:40:22 +0530

    Simple Calculator

--- project1/src/app/app.component.ts
+++ project1/src/app/app.component.ts
@@ -7,8 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project1';
-  res:number;
-  add(num:number,num2:number){
-this.res=num+num2;
+  show: boolean = true;
+  res: number;
+  res1: number;
+  add(num: string, num2: string) {
+    this.res = parseInt(num) + parseInt(num2);
+  }
+  sub(num: string, num2: string) {
+    this.res = parseInt(num) - parseInt(num2);
+  }
+  mul(num: string, num2: string) {
+    this.res = parseInt(num) * parseInt(num2);
+  }
+  div(num: string, num2: string) {
+    this.res = parseInt(num) / parseInt(num2);
+  }
+  modu(num: string, num2: string) {
+    this.res = parseInt(num) % parseInt(num2);
+  }
+  inc(num: string, num2?: string) {
+    if (num2 == undefined) {
+      this.res = parseInt(num) + 1;
+    }
+    else {
+    this.res = parseInt(num) + 1;
+      this.res1 = parseInt(num2) + 1;
+    }
+  }
+  dec(num: string, num2?: string) {
+    
+    if (num2 == undefined) {
+      this.show=false;
+      this.res = parseInt(num) - 1;
+    }
+    else {
+      this.res = parseInt(num) - 1;
+      this.res1 = parseInt(num2) - 1;
+    }
   }
 }
